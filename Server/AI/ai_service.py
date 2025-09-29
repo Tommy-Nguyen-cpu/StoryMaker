0adf3b95cd4297af29a4c4a304f310540357fae2
Tommy-Nguyen <75864631+Tommy-Nguyen-cpu@users.noreply.github.com>
2025-09-29 17:28:36 -0400

    AI Service Startup
    * When starting AI services, we will initialize models to avoid lag when requesting generation.

--- Server/AI/ai_service.py
+++ Server/AI/ai_service.py
@@ -2,13 +2,24 @@ from fastapi import FastAPI
 from typing import Dict
 
 from Server.Schemas.HuggingfaceModel import HuggingfaceModel
+from Server.Schemas.AIModel import BaseAiModel
 
 app = FastAPI()
+
+# AI Models
+llmInstance : BaseAiModel = None
+
+@app.on_event("startup")
+async def startup_event():
+    # Code here runs once at startup
+    print("App is starting up...")
+    global llmInstance
+    llmInstance = HuggingfaceModel("Qwen/Qwen3-4B-Instruct-2507")
+    # e.g., connect to DB, preload models, init resources, etc.
 
 @app.post("/create_story", response_model=Dict[str, str])
 def create_story(request : Dict[str, str]):
 
     print("Creating story with request:", request)
-    HuggingfaceModelInstance = HuggingfaceModel("Qwen/Qwen3-4B-Instruct-2507")
-    story = HuggingfaceModelInstance.generate_text(request['prompt'])
+    story = llmInstance.generate_text(request['prompt'])
     return {"story": story}
